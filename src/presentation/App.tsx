dff554dcba61e7a836106dbc7f581033b215dc41
alvaro-escalante <a.fernandez.naves@gmail.com>
2024-05-27 13:48:15 +0200

    Routas para las diferentes vistas con React Router

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,3 +1,24 @@
+import DetalleEpisodio from './pages/DetalleEpisodio';
+import DetallesPodcast from './pages/DetallePodcast';
+import Principal from './pages/Principal';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Principal />,
+  },
+  {
+    path: 'podcast/:podcastId',
+    element: <DetallesPodcast />,
+  },
+  {
+    path: 'podcast/:podcastId/episode/:episodeId',
+    element: <DetalleEpisodio />,
+  },
+]);
+
+// Router app con Layout como contenedor
 export default function App() {
-  return <App />;
+  return <RouterProvider router={router} />;
 }
---
+++ src/presentation/pages/DetalleEpisodio.tsx
@@ -0,0 +1,3 @@
+export default function DetalleEpisodio() {
+  return <div>DetalleEpisodio</div>;
+}
